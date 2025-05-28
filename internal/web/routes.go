c880084e6bbdbfced65b274086f22ead6b1fbba8
victorposada <victorposadasoto@gmail.com>
2025-05-28 23:00:44 +0200

    templating templates are not templating

--- internal/web/routes.go
+++ internal/web/routes.go
@@ -9,6 +9,7 @@ import (
 	"strings"
 	log "github.com/sirupsen/logrus"
 	"github.com/victorposada/gerrit-wui/internal/gerrit"
+	"bytes"
 )
 
 type Change struct {
@@ -18,10 +19,12 @@ type Change struct {
 type TemplateData struct {
 	GerritURL string
 	Changes   interface{}
+	CustomFieldName *string
+	CustomFieldValue *string
 }
 
 type Board struct {
-    ID int
+    ID string
 	Description string
 	Query string
 	CustomFieldName *string
@@ -35,39 +38,20 @@ type TemplateBoards struct {
 func getBoards(w http.ResponseWriter, r *http.Request) {
 	boards_env := os.Getenv("BOARDS")
 	var boards []Board
-	for i, board := range strings.Split(boards_env, ",") {
+	for _, board := range strings.Split(boards_env, ",") {
 		description, exists_description := os.LookupEnv("DESCRIPTION_" + board)
 		query, exists_query := os.LookupEnv("QUERY_" + board)
-		custom_field_name, exists_custom_field_name := os.LookupEnv("CUSTOM_FIELD_NAME_" + board)
-		custom_full_field_value, exists_custom_field_value := os.LookupEnv("CUSTOM_FIELD_VALUE_" + board)
+		custom_field_name, _ := os.LookupEnv("CUSTOM_FIELD_NAME_" + board)
 
 		if !exists_description || !exists_query {
 			log.Warn("Missing description on query for board "+ board)
 			continue
 		}
-
-		// if (exists_custom_field_name || exists_custom_field_value) && !(exists_custom_field_name && exists_custom_field_value){
-		// 	log.Warn("xor with custom values")
-		// 	boards = append(boards, Board{
-		// 		ID:               i,
-		// 		Description:      description,
-		// 		Query:            query,
-		// 	})
-		// } else{
-		// 	boards = append(boards, Board{
-		// 	ID:               i,
-		// 	Description:      description,
-		// 	Query:            query,
-		// 	CustomFieldName:  &custom_field_name,
-		// 	CustomFieldValue: &custom_full_field_value,
-		// 	})
-		// }
 		boards = append(boards, Board{
-			ID:               i,
+			ID:               board,
 			Description:      description,
 			Query:            query,
 			CustomFieldName:  &custom_field_name,
-			CustomFieldValue: &custom_full_field_value,
 		})
 	}
 
@@ -80,10 +64,23 @@ func getBoards(w http.ResponseWriter, r *http.Request) {
 	tmpl.Execute(w, data)
 }
 
+
 func getBoard(board string) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		GERRIT_URL := os.Getenv("GERRIT_URL")
-		tmpl := template.Must(template.ParseFiles("templates/board.html"))
+		custom_field_name := os.Getenv("CUSTOM_FIELD_NAME_" + board)
+		custom_field_template := (os.Getenv("CUSTOM_FIELD_VALUE_" + board))
+		board_tmpl := template.Must(template.ParseFiles("templates/board.html"))
+
+		var custom_field_value_html string
+		tmpl, err := template.New("custom_value").Parse(custom_field_template)
+		if err != nil {
+			http.Error(w, "Error de plantilla (perfil)", http.StatusInternalServerError)
+			return
+		}
+		buf := new(bytes.Buffer)
+		tmpl.Execute(buf, custom_field_template)
+		custom_field_value_html = buf.String()
 
 		log.Info("got /" + board+ " request")
 		query := os.Getenv("QUERY_" + board)
@@ -92,7 +89,9 @@ func getBoard(board string) http.HandlerFunc {
 		data := TemplateData{
 			GerritURL: GERRIT_URL,
 			Changes:   changes,
+			CustomFieldName: &custom_field_name,
+			CustomFieldValue: &custom_field_value_html,
 		}
-		tmpl.Execute(w, data)
+		board_tmpl.Execute(w, data)
 	}
 }
